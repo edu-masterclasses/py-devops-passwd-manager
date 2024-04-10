dd961849cd8f57b6c3ebebdec178759bdda0e521
Volodymyr Nerovnia <nerv@i.ua>
2024-04-10 23:53:23 +0300

    Added routes: - POST /singup/v1 - GET POST /login/v1 and function verify_plain

--- password.py
+++ password.py
@@ -11,7 +11,51 @@ db_name = 'test.db'
 
 @app.route('/')
 def index():
-  return 'Welkome to Server!'
+  return 'Welcome to Server!'
+
+@app.route('/singup/v1', methods=['POST'])
+def singup_v1():
+  conn = sqlite3.connect(db_name)
+  c = conn.cursor()
+  c.execute('''
+    CREATE TABLE IF NOT EXISTS USER_PLAIN(
+      USERNAME TEXT PROMARY KEY NOT NULL, 
+      PASSWORD TEXT NOT NULL,
+    );
+    ''')
+  conn.commit();
+  try:
+    c.execute("INSERT INTO USER_PLAIN(USERNAME, PASSWORD)"
+    "VALUES('{0}', '{1}')".format(request.form['username'], request['password']))
+    conn.commit();
+  except sqlite3.IntegrityError:
+    return "username has been registrated"
+  print('username:', request.form['username'], ' password: ', request.form['password'])
+  return "signup success  "
+
+
+def verify_plain(username, password):
+  conn = sqlite3.connect(db_name)
+  c = conn.cursor()
+  query = "SELECT PASSWORD FROM USER_PLAIN WHERE USERNAME = '{0}'".format(username)
+  c.execute(query)
+  record = c.fetchone()
+  conn.close();
+  if not records:
+    return False
+  return records[0] == password
+
+@app.route('/login/v1', methods=['GET', 'POST'])
+def login_v1():
+  error = None
+  if request.method == 'POST':
+    if verify_plain(request.form['username'], request.form['password']):
+      error = 'login success'
+    else:
+      error = 'Invalid username or password'
+  else: 
+    error = 'Invalid Method'
+  return error
 
 if __name__ == '__main__' : 
   app.run(host='0.0.0.0', port=5000, ssl_context='adhoc' )
